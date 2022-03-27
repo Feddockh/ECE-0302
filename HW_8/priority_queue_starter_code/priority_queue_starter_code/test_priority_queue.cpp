c777c1d66a1a153d8170c4d3beee43a5045d651b
Feddockh <hayden4feddock@gmail.com>
2022-03-27 15:31:33 -0400

    finished priority queue tests

--- HW_8/priority_queue_starter_code/priority_queue_starter_code/test_priority_queue.cpp
+++ HW_8/priority_queue_starter_code/priority_queue_starter_code/test_priority_queue.cpp
@@ -12,6 +12,25 @@ TEST_CASE("Testing Priority Queue", "[priority queue]") {
 
   PriorityQueueType pq;
 
-  // TODO
-  
+  int max = 20;
+
+  REQUIRE(pq.isEmpty()); // List should be empty
+
+  for (int i=0;i<max;i+=2) { // Insert even values from 0 -> 19
+    pq.add(i);
+    REQUIRE(pq.peek() == i); // Max should be the newest addition in this case
+  }
+  for (int i=1;i<max;i+=2) { // Insert odd values from 0 -> 19
+    pq.add(i);
+    REQUIRE(pq.peek() >= max-2); // Max should either remain the same or increase to max-1 on the last iteration
+  }
+
+  REQUIRE(!pq.isEmpty()); // List should not be empty
+
+  for (int i=max-1;i>=0;i--) { // List should return values from 19 -> 0
+    REQUIRE(pq.peek() == i);
+    pq.remove();
+  }
+
+  REQUIRE(pq.isEmpty()); // List should now be empty
 }
